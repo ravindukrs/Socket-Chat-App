f8b181bb1d9760f1945e18e0d8184061597a4ac4
Ravindu Fernando <ravindupf@gmail.com>
2020-06-15 20:19:15 +0530

    Create Chat Controller with register and send message functions

---
+++ src/main/java/controller/ChatController.java
@@ -0,0 +1,27 @@
+package controller;
+
+import model.Message;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ChatController {
+    //Add User to Chat
+    @MessageMapping("/chat.register")
+    @SendTo("/topic/public")
+    public Message register(@Payload Message message, SimpMessageHeaderAccessor headerAccessor){
+        //Put the current user in header session attribute
+        headerAccessor.getSessionAttributes().put("username", message.getSender());
+        return message;
+    }
+
+    //Send Message
+    @MessageMapping("/chat.send")
+    @SendTo("/topic/public")
+    public Message sendMessage(@Payload Message message){
+        return message;
+    }
+}
