a3ffb8e885036307138acb44fd69275f951ed7cd
Ravindu Fernando <ravindupf@gmail.com>
2020-06-15 20:07:33 +0530

    Initial Commit: Project Initiated with WebSocket. Controller created with SockJS Support

---
+++ src/main/java/config/WebsocketConfig.java
@@ -0,0 +1,29 @@
+package config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebsocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    //Configure STOMP Endpoints
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        //Provide Prefix URL
+        //Use SockJS
+        registry.addEndpoint("/chats").withSockJS();
+
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        //Enable Configure Message Broker
+        registry.enableSimpleBroker("/topic");
+        registry.setApplicationDestinationPrefixes("/app");
+    }
+}
---
+++ src/test/java/com/example/socketlovers/SocketloversApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.socketlovers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocketloversApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
